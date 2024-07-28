b47f656bb6b130135cd2df685b02a7551e4eedc8
Ray Liu <rray12tw346@gmail.com>
2024-07-29 00:42:08 +0800

    加入簡易ws連線

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 interface Message {
   id: number
@@ -7,18 +8,27 @@ interface Message {
   timestamp: Date
 }
 
-export const useChatStore = defineStore('chat', {
-  state: () => ({
-    messages: [] as Message[],
-    channels: ['general', 'random', 'help'] as string[],
-    currentChannel: 'general'
-  }),
-  actions: {
-    addMessage(message: Message) {
-      this.messages.push(message)
-    },
-    setCurrentChannel(channel: string) {
-      this.currentChannel = channel
-    }
+export const useChatStore = defineStore('chat', () => {
+  const messages = ref<Message[]>([])
+  const channels = ref(['general', 'random', 'help'])
+  const currentChannel = ref('general')
+  const socket = new WebSocket('ws://localhost:8080/ws')
+
+  socket.onmessage = (event: MessageEvent) => {
+    const message = JSON.parse(event.data)
+    console.log(message)
   }
+
+  function addMessage(message: Message) {
+    messages.value.push(message)
+
+    // Send the message to the server
+    socket.send(JSON.stringify(message))
+  }
+
+  function setCurrentChannel(channel: string) {
+    currentChannel.value = channel
+  }
+
+  return { messages, channels, currentChannel, addMessage, setCurrentChannel }
 })
